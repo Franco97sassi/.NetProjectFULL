d44ac3605fe069f7a69d2b69f051decd10226106
franco97sassi <franco.sassi97@gmail.com>
2026-06-15 14:46:19 -0300

    pendientes de biblioteca .net

--- BibliotecaApp.Etapa9/src/Services/Catalog/Catalog.Api/Program.cs
+++ BibliotecaApp.Etapa9/src/Services/Catalog/Catalog.Api/Program.cs
@@ -29,7 +29,8 @@ builder.Services.AddMassTransit(x =>
         cfg.ConfigureEndpoints(context);
     });
 });
-
+builder.Services.AddMemoryCache();
+builder.Services.AddResponseCompression();
 var app = builder.Build();
 app.UseSwagger(); app.UseSwaggerUI();
 app.MapControllers();
